67eaa110ffaf9cbc96f5fb9f91ecb673b9588c55
plexion <46572320+plexiondev@users.noreply.github.com>
2022-01-02 22:16:41 +0000

    YEP

--- js/options.js
+++
@@ -1,9 +0,0 @@
-function openOptions() {
-    var x = document.getElementById("options-menu");
-
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
